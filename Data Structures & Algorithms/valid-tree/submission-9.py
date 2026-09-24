3075b0b12e033f22206b5502546e532461690c84
Riccardo Tomassoli <111280428+riccardo1512@users.noreply.github.com>
2026-09-24 16:39:09 +0200

    Add: valid-tree - submission-9

---
+++ Data Structures & Algorithms/valid-tree/submission-9.py
@@ -0,0 +1,23 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        graphMap = {i:[] for i in range(n)}
+        visited = set()
+
+        for a, b in edges:
+            graphMap[a].append(b)
+            graphMap[b].append(a)
+        
+        def dfs(i, parent):
+            if i in visited:
+                return False
+            
+            visited.add(i)
+            for j in graphMap[i]:
+                if j != parent and not dfs(j, i):
+                    return False
+            return True
+        
+        if not dfs(0, -1):
+            return False
+        
+        return len(visited) == n
